9170e2c785fa2f6471f3483cdc7d965ef8062d1f
justinthestickman <jchuang@NN123-HA-22.HS.sd203.org>
2015-10-30 14:47:48 -0500

    Chapter 6 Day 4

--- Chapter 6 Practice/Diamond.java
+++ Chapter 6 Practice/Diamond.java
@@ -7,6 +7,44 @@ public class Diamond
         Scanner scan = new Scanner( System.in );
         System.out.print("Enter an integer: ");
         int input = scan.nextInt();
-        String substring = "";
+        int lines = (input * 2) - 1;
+        int lines1 = input;
+        int lines2 = input-1;
+        for (int i = 0; i < lines1; i++)
+        {
+            int spaces = (lines1 - i);
+            int asterisks = (i * 2) + 1;
+            for (int n = 0; n < spaces; n++)
+            {
+                System.out.print(" ");
+            }
+            for (int x = 0; x < asterisks; x++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+        for (int i = lines2; i > 0; i--)
+        {
+            int spaces = (lines1 - i + 1);
+            int asterisks = (i * 2) - 1;
+            for (int n = 0; n < spaces; n++)
+            {
+                System.out.print(" ");
+            }
+            for (int x = 0; x < asterisks; x++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
     }
 }
+
+//   *   
+//  ***  
+// *****  
+//*******
+// *****  
+//  ***  
+//   *   
