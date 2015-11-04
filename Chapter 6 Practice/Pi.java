fbe5002fc3f2c8bc12a14fc1b043a6a973f513d0
justinthestickman <jchuang@NN123-HA-22.HS.sd203.org>
2015-11-04 14:43:10 -0600

    Pi

--- Chapter 6 Practice/Pi.java
+++ Chapter 6 Practice/Pi.java
@@ -5,11 +5,11 @@ public class Pi
         final double NUM = 4.0;
         double denom = 1.0;
         double pi = 0.0;
-        for (int i = 1; i <= 10000; i++)
+        for (long i = 1; i <= 1000000000; i++)
         {
             pi += (NUM / denom);
             denom += 2;
-            pi -= (NUM / denom);
+            pi -= (NUM / denom); 
             denom += 2;
         }
         System.out.println(pi);
