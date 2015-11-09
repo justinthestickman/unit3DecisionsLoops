4caaef45f74467225b39e2403aae0758bb914d49
justinthestickman <jchuang@NN123-HA-22.HS.sd203.org>
2015-11-09 15:09:58 -0600

    GOL D1

--- GameOfLifeLab/GameOfLifeTest.java
+++ GameOfLifeLab/GameOfLifeTest.java
@@ -47,10 +47,10 @@ public class GameOfLifeTest
          *  (X: alive; -: dead)
          * 
          *    0 1 2 3 4
-         *  0 - - X - -
-         *  1 - - - - -
-         *  2 X X - - -
-         *  3 - - - - -
+         *  0 - - - - -
+         *  1 - - X X -
+         *  2 - X X - X
+         *  3 - - X X -
          *  4 - - - - -
          *  
          */
@@ -67,9 +67,14 @@ public class GameOfLifeTest
                 Actor cell = game.getActor(row, col);
 
                 // if the cell at the current row and col should be alive, assert that the actor is not null
-                if(     (row == 0 && col == 2) ||
-                        (row == 2 && col == 0) ||
-                        (row == 2 && col == 1))
+                if(     (row == 1 && col == 2) ||
+                        (row == 1 && col == 3) ||
+                        (row == 2 && col == 1) ||
+                        (row == 2 && col == 2) ||
+                        (row == 2 && col == 4) ||
+                        (row == 3 && col == 2) ||
+                        (row == 3 && col == 3)
+                        )
                 {
                     assertNotNull("expected alive cell at (" + row + ", " + col + ")", cell);
                 }
@@ -84,7 +89,8 @@ public class GameOfLifeTest
     @Test
     public void testFinalState()
     {
-        /* verify that the actual pattern matches the expected pattern after 3 generations         *  
+        /* verify that the actual pattern matches the expected pattern after 3 generations         
+         *  
          */
         
         // ...
